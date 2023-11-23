1a7bf011ca5836c868b009b154341894ab5a17fe
DESKTOP-9KOFLCP\USER <csm5588@gmail.com>
2023-11-24 08:52:23 +0900

    init project

---
+++ Chapter3/Ch3Lab1/Ch3Lab1/Inc/myheader.h
@@ -0,0 +1,17 @@
+/*
+ * myheader.h
+ *
+ *  Created on: Nov 15, 2023
+ *      Author: USER
+ */
+
+#ifndef MYHEADER_H_
+#define MYHEADER_H_
+
+#include <stdbool.h>
+enum {
+	FALSE,
+	TRUE
+};
+
+#endif /* MYHEADER_H_ */
